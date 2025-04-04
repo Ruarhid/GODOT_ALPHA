c02edf00aa8f090ddca928336d41f565d6ef80bd
Kiberlis <Kiberlis3000@gmail.com>
2025-04-04 18:57:13 +0700

    Alpha

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -26,7 +26,7 @@ func _on_play_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/level.tscn")
 
 func _on_options_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/options.tscn")
+	get_tree().change_scene_to_file("res://scenes/options_menu.tscn")
 
 func _on_about_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/about.tscn")

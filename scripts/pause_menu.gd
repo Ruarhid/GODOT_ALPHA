441dc7a724c5706f12318a3e086c3e96e3d13399
Kiberlis <Kiberlis3000@gmail.com>
2025-04-06 20:15:32 +0700

    add menu pause menu

--- scripts/pause_menu.gd
+++ scripts/pause_menu.gd
@@ -3,9 +3,11 @@ class_name PauseMenu
 
 @onready var resume_button: Button = %ResumeButton
 @onready var exit_button: Button = %ExitButton
+@onready var main_menu_button: Button = %MainMenuButton
 
 func  _ready() -> void:
 	resume_button.pressed.connect(_on_resume_pressed)
+	main_menu_button.pressed.connect(_on_main_menu_pressed)
 	exit_button.pressed.connect(_on_exit_pressed)
 	hide() #  Скрыть меню при запуске
 	process_mode = PROCESS_MODE_ALWAYS # Обрабатывать меню даже на пузе
@@ -23,6 +25,10 @@ func _on_resume_pressed() -> void:
 	hide()
 	get_tree().paused = false # Снимаем паузу
 
+func _on_main_menu_pressed() -> void:
+	get_tree().paused = false
+	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
+
 func _on_exit_pressed() -> void:
 	get_tree().paused = false # Снимаем паузу перед выходом
-	get_tree().quit() # Выход из игры  \ # get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
+	get_tree().quit() # Выход из игры
